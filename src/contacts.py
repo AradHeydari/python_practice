37c1c2d1f849f34635064f47c71327597517971c
Arad Heydari <aradheydari1385@gmail.com>
2026-07-07 11:58:38 +0330

    Commit 4: REFACTOR - add type hints

--- src/contacts.py
+++ src/contacts.py
@@ -1,6 +1,6 @@
 class ContactsBook:
     def __init__(self):
-        self.contacts = []
+        self.contacts : list[dict[str, str]] = []
 
-    def add_contact(self, name, phone):
+    def add_contact(self, name: str, phone: str) -> None:
         self.contacts.append({"name": name, "phone": phone})
