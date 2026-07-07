f96941990a1a26316e093552b46e4a1f9f282a1f
Arad Heydari <aradheydari1385@gmail.com>
2026-07-07 12:07:17 +0330

    Commit 5: RED - test for list_contacts

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -6,5 +6,10 @@ class test_contacts_book(unittest.TestCase):
         book = ContactsBook()
         # name and phone number are fake and not real
         book.add_contact("John", "09004582971")
-        self.assertEqual(len(book,contacts), 1)
+        self.assertEqual(len(book.contacts), 1)
         
+    def test_list_contacts(self):
+        book = ContactsBook()
+        # name and phone number are fake and not real
+        book.add_contact("Sara", "09010982457")
+        self.assertEqual(book.list_contacts(), [{"name": "Sara","phone": "09010982457"}])
