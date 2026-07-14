af6aace9e4ccf619b4acd74161b12553c2dcb820
Arad Heydari <aradheydari1385@gmail.com>
2026-07-14 15:32:23 +0330

    Commit 14: add menu for application to run

--- src/contacts.py
+++ src/contacts.py
@@ -23,3 +23,65 @@ class ContactsBook:
     def load_file(self, filename):
         with open(filename, "r") as f:
             self.contacts = json.load(f)
+
+
+
+
+def main():
+    book = ContactsBook()
+    try:
+        book.load_file("contacts.json")
+        print("information loaded")
+    except FileNotFoundError:
+        print("no file found. new book added")
+
+    while True:
+    
+        print("\n contacts book")
+        print("1. add contact")
+        print("2. show all contacts")
+        print("3. search contact")
+        print("4. delete contact")
+        print("5. save and exit")
+        choice = input("chose from (1-5): ")
+
+        if choice == "1":
+            name = input("name: ")
+            phone = input("phone number: ")
+            book.add_contact(name, phone)
+            print("contact added")
+
+        elif choice == "2":
+            contacts = book.list_contacts()
+            if not contacts:
+                print("book is empty")
+            else:
+                for i, c in enumerate(contacts, 1):
+                    print(f"{i}. {c['name']} - {c['phone']}")
+
+        elif choice == "3":
+            xyz = input("enter name or phone number:")
+            results = book.search(xyz)
+            if results:
+                for c in results:
+                    print(f"--> {c['name']} - {c['phone']}")
+            else:
+                print("no contacts added")
+
+        elif choice == "4":
+            xyz = input("name or phone number: ")
+            book.delete(xyz)
+            print("contact has been deleted")
+
+        elif choice == "5":
+            book.save_to_file("contacts.json")
+            print("information has been saved. \n Goodbye")
+            break
+
+        else:
+            print("invalid choice. \n try again")
+
+    
+if __name__ == "__main__":
+    main()
+
